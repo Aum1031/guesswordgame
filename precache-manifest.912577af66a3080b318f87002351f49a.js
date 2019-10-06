7a0089d78ad3c4e19745a3def707b7fe4f1c94b9
Aum Dutta <saurabhaumdutta@gmail.com>
2019-10-06 00:08:46 -0400

    Updates

---
+++ precache-manifest.912577af66a3080b318f87002351f49a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee1e0557dcaf224270d4954f0a2589e",
+    "url": "/guesswordgame/index.html"
+  },
+  {
+    "revision": "eb9e2214a1c20b4ddf3e",
+    "url": "/guesswordgame/static/css/main.18f9b0c5.chunk.css"
+  },
+  {
+    "revision": "50b0f4c1867d4bb8e727",
+    "url": "/guesswordgame/static/js/2.9b1d42ab.chunk.js"
+  },
+  {
+    "revision": "eb9e2214a1c20b4ddf3e",
+    "url": "/guesswordgame/static/js/main.1409ae6e.chunk.js"
+  },
+  {
+    "revision": "af3cbf287124c6f4bf47",
+    "url": "/guesswordgame/static/js/runtime-main.8da0b39c.js"
+  }
+]);
